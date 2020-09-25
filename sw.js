a0853914a6bef4df1791b6cca71a4edc5f02e227
qzwangUSTC <1837597423@qq.com>
2020-09-25 22:04:38 +0800

    Site updated: 2020-09-25 22:04:36

--- sw.js
+++ sw.js
@@ -24,21 +24,19 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/posts/4053.html');
-
   precacheUrls.push('/posts/61265.html');
 
   precacheUrls.push('/posts/37102.html');
 
+  precacheUrls.push('/posts/49384.html');
+
   precacheUrls.push('/posts/48969.html');
 
-  precacheUrls.push('/posts/35327.html');
-
-  precacheUrls.push('/posts/49384.html');
+  precacheUrls.push('/posts/4053.html');
 
   precacheUrls.push('/posts/22036.html');
 
-  precacheUrls.push('/posts/7667.html');
+  precacheUrls.push('/posts/35327.html');
 
   precacheUrls.push('/posts/20554.html');
 
